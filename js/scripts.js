581ec1fbdad840df214bfda3d51f669fdd0928b1
Mike H <72580577+mekanik20@users.noreply.github.com>
2020-11-16 18:38:39 -0500

    Update scripts.js
    Added an array for a list of 3 pokemon with 3 keys added to each object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-alert('Hello world');
+//pokemon array
+let myVariable = 'pokemonList';
+console.log(typeof myVariable);
+let pokemonList = [
+    {name: 'Arcanine';
+    height: 1.9;
+    type: 'flashfire', 'intimidate', 'justified',},
+    {name: 'Aggron';
+    height: 2.1;
+    type: 'sturdy', 'rock-head', 'heavy-metal',},
+    {name: 'Scyther'
+    height: 1.5
+    type: 'swarm', 'steadfast', 'technician'}
+];
 
-var favoriteFood = 'Mexican';
-document.write(favoriteFood);

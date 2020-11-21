88740f2d4e7c5ae6258cecd1674ea5db9e5b6607
Mike H <72580577+mekanik20@users.noreply.github.com>
2020-11-20 19:48:27 -0500

    Update scripts.js
    updated syntax for better readability

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@
 let myVariable = 'pokemonList';
 console.log(typeof myVariable);
 let pokemonList = [
-    {name: 'Arcanine';
-    height: 1.9;
-    type: 'flashfire', 'intimidate', 'justified',},
-    {name: 'Aggron';
-    height: 2.1;
-    type: 'sturdy', 'rock-head', 'heavy-metal',},
-    {name: 'Scyther'
-    height: 1.5
-    type: 'swarm', 'steadfast', 'technician'}
+    {
+      name: 'Arcanine',
+      height: 1.9,
+      type: ['flashfire', 'intimidate', 'justified']
+    },
+    {
+      name: 'Aggron',
+      height: 2.1,
+      type: ['sturdy', 'rock-head', 'heavy-metal']
+    },
+    {
+      name: 'Scyther',
+      height: 1.5,
+      type: ['swarm', 'steadfast', 'technician']
+    }
 ];
 

4a9b56edfa12d294e4bf2cbcf440bc3d4a29325b
Mike H <72580577+mekanik20@users.noreply.github.com>
2020-12-07 20:19:05 -0500

    Update scripts.js
    updated code to define specific pokemon as a big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,27 @@ let pokemonList = [
     {
       name: 'Arcanine ',
       height: 1.9,
-      types: ['flashfire', 'intimidate', 'justified']
+      types: ['flashfire', 'intimidate', 'justified'],
     },
     {
       name: ' Aggron ',
       height: 2.1,
-      types: ['sturdy', 'rock-head', 'heavy-metal']
+      types: ['sturdy', 'rock-head', 'heavy-metal'],
     },
     {
       name: ' Scyther ',
       height: 1.5,
-      types: ['swarm', 'steadfast', 'technician']
-    }
+      types: ['swarm', 'steadfast', 'technician'],
+    },
 ];
-
+// for loop for pokemonList
 for (let i = 0; i < pokemonList.length; i++){
     const pokemon = pokemonList[i];
     console.log(pokemon);
     document.write(pokemon.name + pokemon.height);
-}
 // define specific pokemon as a big pokemon
-let pokemon = {name: 'Aggron', height: 2.1}
-
-if (pokemon.height > 2.0 || pokemon === 1){
-    console.log(' Wow, that\'s big!');
-    document.write(pokemon + 'Wow, that\'s big!');
+if (pokemon.height > 2.0) {
+    console.log("Wow, that\'s big!");
+    document.write("-" + "Wow that\'s big!")
   }
+}

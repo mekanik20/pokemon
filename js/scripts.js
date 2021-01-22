f878b881f79facf76251e10c3bd3c93f96d48791
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-01-21 20:18:00 -0500

    1.6 task
    created buttons for pokemon and  added click events for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,76 @@
 //pokemon array IIFE
-let myVariable = 'pokemonList';
-console.log(typeof myVariable);
+//let myVariable = 'pokemonList';
+//console.log(typeof myVariable);
 let pokemonRepository = (function () {
   let pokemonList = [
     {
-      name: ' Arcanine- ',
+      name: ' Arcanine ',
       height: 1.9,
       types: [' flashfire', ' intimidate', ' justified'],
     },
     {
-      name: ' Aggron- ',
+      name: ' Aggron ',
       height: 2.1,
       types: [' sturdy', ' rock-head', ' heavy-metal'],
     },
     {
-      name: ' Scyther- ',
+      name: ' Scyther ',
       height: 1.5,
       types: [' swarm', ' steadfast ', ' technician'],
     }
   ];
-  
-  function add(item) {
-    pokemonList.push(item);
+ 
+  //adding of pokemon(s)
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is incorrect");
+    }
   }
 
+  //show the pokemon list
   function getAll() {
     return pokemonList;
   }
-  
+
+  //buttons added for pokemon list
+  function addListItem(pokemon){
+    let pokemonFile = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+    listpokemon.appendChild(button);
+    pokemonFile.appendChild(listpokemon);  
+   // event listener for button click
+    button.addEventListener('click', function(event){
+      showDetails(pokemon.name)
+      showDetails(pokemon)
+    })
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon)
+  }
+ 
+  //for adding new pokemon into the array
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-// forEach() loop logging details of pokemonList
-pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write("<h1>" + pokemon.name + pokemon.types + "</h1>")
-});
+pokemonRepository.add({ name: 'Pikachu', height: 0.3, types: [' electric'] });
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll());
 
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+  });

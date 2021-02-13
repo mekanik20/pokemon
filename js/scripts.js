1b8a5ba8768f35b4e37c74f3b014d7f016ccd169
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-02-13 13:15:05 -0500

    First attempt task 1.10
    Added load-list, modal, and button to index. First attempt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 //pokemon array IIFE
-//let myVariable = 'pokemonList';
-//console.log(typeof myVariable);
-//loadList() and loadDetails() added
 let pokemonRepository = (function () {  
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
@@ -32,13 +29,15 @@ let pokemonRepository = (function () {
     button.innerText = pokemon.name;
     button.classList.add("button");
     listpokemon.appendChild(button);
-    pokemonList.appendChild(listpokemon);  
+    pokemonList.appendChild(listpokemon);
+
    // event listener for button click
     button.addEventListener("click", function (event) {
       showDetails(pokemon);
     });
   }
 
+  //load list for pokemon from URL
   function loadList(pokemon) {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -56,6 +55,7 @@ let pokemonRepository = (function () {
     });
   }
 
+  //load the details for each pokemon from the URL
   function loadDetails(pokemon) {
     let url = pokemon.detailsUrl;
     return fetch(url).then(function (response) {
@@ -73,92 +73,15 @@ let pokemonRepository = (function () {
     });
   }
  
+  //function for loadDetails and showDetails
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function () {
       showModal(pokemon);
     });
   }
  
-//register and validation for email and password
-// (function() {
-//   let form = document.querySelector('#register-form');
-//   let emailInput = document.querySelector('#email');
-//   let passwordInput = document.querySelector ('#password');
 
-//   function showErrorMessage(input, message) {
-//     let container = input.parentElement;
-//     let error = container.querySelector('.error-message');
-//     if (error) {
-//       container.removeChild(error);
-//     }
-//     if (message) {
-//       let error = document.createElement('div');
-//       error.classList.add('error-message');
-//       error.innerText = message;
-//       container.appendChild(error);
-//     }
-//   }
-
-//   function validateEmail() {
-//    let value = emailInput.value;
-//    if (!value) {
-//      showErrorMessage(emailInput, 'Email is a required field.');
-//      return false;
-//    }
-
-//    if (value.indexOf('@') === -1) {
-//      showErrorMessage(emailInput, 'You must enter a valid email address.');
-//      return false;
-//    }
-
-//    if (value.indexOf('.') === -1) {
-//      showErrorMessage(emailInput, 'You must enter a valid email address.')
-//      return false;
-//     //return value && hasAtSign && hasDot;
-//    }
-
-//    showErrorMessage(emailInput, null);
-//    return true;
-//   }
-
-//   function validatePassword() {
-//    let value = passwordInput.value;
-
-//    if (!value) {
-//      showErrorMessage(passwordInput, 'Password is a required field');
-//      return false;
-//    }
-
-//    if (value.length < 8){
-//      showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
-//      return false;
-//    }
-  
-//    showErrorMessage(passwordInput, null);
-//    return true;
-//   }
-
-//   function validateForm() {
-//     let isValidEmail = validateEmail();
-//     let isValidPassword = validatePassword();
-//     return isValidEmail && isValidPassword;
-//   }
-
-//   form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     if (valiidateForm()) {
-//       alert('Success!');
-//     }
-//   });
-// }());
-
-//   emailInput.addEventListener('input', validateEmail);
-//   passwordInput.addEventListener('input', validatePassword);
-
-
-//modal dialog
-// Modal insert
-//let pokemonRepository = (function() {
+  //modal creation point
   let modalContainer = document.querySelector("#modal-container");
   function showModal(pokemon) {
     modalContainer.innerHTML = "";
@@ -194,41 +117,7 @@ let pokemonRepository = (function () {
     modalContainer.classList.remove("is-visible");
   }
 
-  //   if (dialogPromiseReject) {
-  //     dialogPromiseReject();
-  //     dialogPromiseReject = null;
-  //   }
-  // }
-
-  // function showDialog(title, text) {
-  //   showModal(title, text);
-  //   let modal = modalContainer.querySelector('.modal');
-
-  //   let confirmButton = document.createElement('button');
-  //   confirmButton.classList.add('modal-confirm');
-  //   confirmButton.innerText = 'Confirm';
-
-  //   let cancelButton = document.createElement('button');
-  //   cancelButton.classList.add('modal-cancel');
-  //   cancelButton.innerText = 'Cancel';
-
-  //   modal.appendChild(confirmButton);
-  //   modal.appendChild(cancelButton);
-
-  //   confirmButton.focus();
-
-  //   return new Promise((resolve, reject) => {
-  //     cancelButton.addEventListener('click', hideModal);
-  //     confirmButton.addEventListener('click', () => {
-  //       dialogPromiseReject = null; 
-  //       hideModal();
-  //       resolve();
-  //     });
-
-  //     dialogPromiseReject = reject;
-  //   });
-  // }
-
+  //eventListener for escape key pressed or click event to exit modal
   window.addEventListener("keydown", (e) => {
     if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
       hideModal();
@@ -241,17 +130,6 @@ let pokemonRepository = (function () {
     }
   });
 
-  // document.querySelector('#show-modal').addEventListener('click', () => {
-  //   showModal('My Modal', 'This is my modal, there are many like it but this one is mine.');
-  // });
-
-  // document.querySelector('#show-dialog').addEventListener('click', () => {
-  //   showDialog('Confirm action', 'Are you sure you want to do this?').then(function() {
-  //     alert('confirmed!');
-  //   }, () => {
-  //     alert('not confirmed');
-  //   });
-  // });
   return {
     add: add,
     getAll: getAll,
